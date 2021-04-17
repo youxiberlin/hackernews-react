f2dc667275d3082679964ff2159c3970b75ccf06
Yuki Sato <yuki.sato@lightcurve.io>
2021-04-17 15:02:04 +0200

    Fetch and render top stories on Home

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,35 @@
+import { useState, useEffect } from 'react';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Link,
 } from "react-router-dom";
+import axios from 'axios'
 
 import Home from './Home';
 import ItemDetail from './ItemDetail';
 
 const App = () => {
+  const [stories, setStories] = useState([]);
+  useEffect(() => {
+    const getTopStories = async () => {
+      const { data } = await axios.get('https://hacker-news.firebaseio.com/v0/topstories.json');
+      const topStoryIds = data.slice(0, 30);
+      const getItemPromises = topStoryIds.map(id => axios.get(`https://hacker-news.firebaseio.com/v0/item/${id}.json`));
+      const items = await Promise.all(getItemPromises);
+      const itemsData = items.map(item => item.data);
+      setStories(itemsData);
+    };
+    getTopStories();
+  }, []);
+
   return (
     <Router>
         <Link to="/">Hacker News - Version Yuki</Link>
       <Switch>
         <Route exact path="/">
-          <Home />
+          <Home stories={stories}/>
         </Route>
         <Route path="/item/:itemId">
           <ItemDetail />
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,30 @@
-const Home = () => {
+import { Link } from 'react-router-dom';
+
+const Home = ({ stories }) => {
+  const renderStories = stories.map(story => {
+    return (
+      <li key={story.id} className="mt-1">
+        <div>
+          <a className="item" href={story.url}>
+            {story.title}
+          </a>
+        </div>
+        <div className="text-sm text-secondary">
+          {story.score} points by {story.by} xx hours ago
+          <Link to={`item/${story.id}`}>
+          {story.descendants} 
+          </Link>
+          comments
+        </div>
+      </li>
+    );
+  });
   return (
-    <div>Home</div>
+    <div className="container bg-light">
+      <ol>
+      {renderStories}
+      </ol>
+    </div>
   );
 };
 
