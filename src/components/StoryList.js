02a6a92188f9a5d17c10a11d1a25203f023c7ec9
Yuki Sato <yuki.sato@lightcurve.io>
2021-04-19 23:17:45 +0200

    Add error handling for Promise.all

--- src/components/StoryList.js
+++ src/components/StoryList.js
@@ -4,9 +4,11 @@ const StoryList = ({ stories, pageType, page }) => {
   return (
     <ol className="pl-4" start={page ? (+page * 30) + 1 : null}>
       {stories.map(story => (
-        <li key={story.id} className="mt-1">
-          <StoryItem key={story.id} story={story} pageType={pageType} />
-        </li>))}
+        story ? (
+          <li key={story.id} className="mt-1">
+            <StoryItem key={story.id} story={story} pageType={pageType} />
+          </li>
+        ) : null))}
     </ol>
   )
 };
--- src/helper/getTopStories.js
+++ src/helper/getTopStories.js
@@ -3,7 +3,13 @@ import axios from 'axios';
 const getTopStories = async (page) => {
   const { data: topStories } = await axios.get('https://hacker-news.firebaseio.com/v0/topstories.json');
   const topStoryIds = topStories.slice(+page * 30, (+page + 1) * 30);
-  const getItemPromises = topStoryIds.map(id => axios.get(`https://hacker-news.firebaseio.com/v0/item/${id}.json`));
+  const catchErr = p => p.catch(err => {
+    console.log('error', err)
+    return { data: {}}
+  });
+  const getItemPromises = topStoryIds
+    .map(id => axios.get(`https://hacker-news.firebaseio.com/v0/item/${id}.json`))
+    .map(promise => catchErr(promise))
   const items = await Promise.all(getItemPromises);
   const itemsData = items.map(item => item.data);
   return itemsData;
