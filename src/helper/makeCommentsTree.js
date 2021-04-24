a191437c2a7e4ca4b31a4d938c7e0dac3086f31b
Yuki Sato <yuki.sato@lightcurve.io>
2021-04-24 10:24:55 +0200

    Use sessionStorage inside makeCommentsTree

--- src/helper/makeCommentsTree.js
+++ src/helper/makeCommentsTree.js
@@ -1,28 +1,47 @@
 import axios from 'axios';
 import { indexBy, prop } from 'ramda';
 
+const myStorage = window.sessionStorage;
+
 const makeCommentsTree = async (parent, result = {}) => {
+  const newKids = [];
   const kidsIdArr = parent.kids;
+
+  const storedComments = [];
+  for (let i = 0; i < kidsIdArr.length; i++) {
+    const kidId = kidsIdArr[i];
+    const storedKidData = myStorage.getItem(kidId);
+    if (storedKidData) {
+      storedComments.push(JSON.parse(storedKidData));
+    } else {
+      newKids.push(kidId);
+    }
+  }
+
   const catchErr = p => p.catch(err => {
     console.log('error', err);
     return { data: {}};
   });
-  const getCommentPromises = kidsIdArr
+
+  const getCommentPromises = newKids
     .map(id => axios.get(`https://hacker-news.firebaseio.com/v0/item/${id}.json`))
     .map(promise => catchErr(promise));
-  const comments = await Promise.all(getCommentPromises);
-  const commentsData = comments.map(item => item.data);
-  const commentsMap = indexBy(prop('id'), commentsData);
+  const newComments = await Promise.all(getCommentPromises);
+  const newCommentsData = newComments.map(item => item.data);
+  const allComments = [...new Set([...storedComments, ...newCommentsData])];
+  const commentsMap = indexBy(prop('id'), allComments);
   result.kids = commentsMap;
 
-  for (let i = 0; i < commentsData.length; i++){
-    const item = commentsData[i];
+  for (let i = 0; i < allComments.length; i++){
+    const item = allComments[i];
+    myStorage.setItem(item.id, JSON.stringify(item));
     if (item.kids) {
       await makeCommentsTree(item, result.kids[item.id]);
     }
   }
 
   return result;
+
 }
 
 export default makeCommentsTree;
